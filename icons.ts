3a4d303e92a57585608a6b75fea206bcd1912edb
sameer0541 <151599264+sameer0541@users.noreply.github.com>
2025-02-06 22:40:39 +0530

    Add files via upload

---
+++ icons.ts
@@ -0,0 +1,18 @@
+import arrow from "../assets/icons/arrow.png";
+import alarm from "../assets/icons/alarm.png"
+import clock from "../assets/icons/clock.png"
+import flag from "../assets/icons/flag.png"
+import location from "../assets/icons/location.png"
+import play from "../assets/icons/play.png"
+import plus from "../assets/icons/PLUS.png"
+import stopwatch from "../assets/icons/STOPWATCH.png"
+import timer from "../assets/icons/timer.png"
+import testIcon from '../assets/images/favicon.png'
+import background from '../assets/images/back.png'
+import temp from '../assets/icons/temp.png'
+import pause from '../assets/icons/pause.png'
+import reset from '../assets/icons/reset.png'
+
+export default {
+    arrow,alarm,clock,flag,location,play,plus,stopwatch,timer,testIcon,background,temp,pause,reset
+}
